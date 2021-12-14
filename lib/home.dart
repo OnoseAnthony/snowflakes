3ab3f230e2c6048464461c27e1626b7e00ea270b
Stranger3145 <anthony@troveapp.co>
2021-12-14 11:43:30 +0100

    added an animation background to serve as a layer for the snowflakes. flakes creation [WIP] snow storm color gradient [WIP]

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+              child: MirrorAnimation<Color?>(
+                tween: ColorTween(begin: Colors.lightBlueAccent.shade200, end: Colors.lightBlue.shade500),
+                duration: const Duration(seconds: 3),
+                curve: Curves.easeInOut,
+                builder: (context, child, value) {
+                  return Container(
+                    color: value
+                  );
+                },
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
